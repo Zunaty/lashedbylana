ea851ba75dd97126882ec6cebf57dfdeaf3c07bd
Victor Perez <zunaty@gmail.com>
2021-12-25 17:58:34 -0700

    Front end login and signup route setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import * as React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import auth from './utils/auth';
+
+// MUI Theme Creation
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 // Importing components to app
@@ -8,7 +14,30 @@ import Nav from './components/Nav';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const loggedIn = auth.loggedIn();
+
+// Light mode theme colors
 const theme = createTheme({
   palette: {
     type: 'light',
@@ -27,6 +56,7 @@ const theme = createTheme({
   }
 });
 
+// Dark mode theme colors
 const darkTheme = createTheme({
   palette: {
     type: 'dark',
@@ -47,14 +77,26 @@ const darkTheme = createTheme({
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <Nav />
-        <Home />
-        <Login />
-        <Signup />
-      </div>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <header>
+          <Nav />
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<NoMatch />} />
+          </Routes>
+        </main>
+
+        <footer>
+
+        </footer>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
